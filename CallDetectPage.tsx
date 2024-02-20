ba55bc7606b6fb16701b59266f15435c2822273f
ashirkhan64 <pmashirkhan@gmail.com>
2024-02-20 19:05:16 +0530

    testPage created

---
+++ CallDetectPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+
+const CallDetectPage = () => {
+    return (
+        <View>
+            <Text>Call Detect</Text>
+        </View>
+    )
+}
+
+export default CallDetectPage
